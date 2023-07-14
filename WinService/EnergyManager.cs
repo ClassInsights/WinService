b260d91c64a9e874e0bf877f5e631db90209a3e1
Julian <54598714+akaJuliaan@users.noreply.github.com>
2023-07-14 13:47:55 +0200

    Create EnergyManager.cs

---
+++ WinService/EnergyManager.cs
@@ -0,0 +1,47 @@
+﻿using LibreHardwareMonitor.Hardware;
+
+namespace WinService;
+
+public class EnergyManager : IDisposable
+{
+    private readonly Computer _computer;
+
+    public class UpdateVisitor : IVisitor
+    {
+        public void VisitComputer(IComputer computer)
+        {
+            computer.Traverse(this);
+        }
+        public void VisitHardware(IHardware hardware)
+        {
+            hardware.Update();
+            foreach (var subHardware in hardware.SubHardware) subHardware.Accept(this);
+        }
+        public void VisitSensor(ISensor sensor) { }
+        public void VisitParameter(IParameter parameter) { }
+    }
+
+    public EnergyManager()
+    {
+        _computer = new Computer
+        {
+            IsCpuEnabled = true
+        };
+
+        _computer.Open();
+    }
+
+    // returns sum of all cpu energy
+    public float GetCpuEnergy()
+    {
+        _computer.Accept(new UpdateVisitor());
+        return _computer.Hardware.Where(x => x.HardwareType == HardwareType.Cpu).Aggregate<IHardware?, float?>(0f, (current, hardware) => current + hardware?.Sensors.Where(x => x.SensorType == SensorType.Power).Sum(x => x.Value)) ?? 0;
+    }
+
+    public void Dispose()
+    {
+        _computer.Close();
+        GC.SuppressFinalize(this);
+    }
+}
+
