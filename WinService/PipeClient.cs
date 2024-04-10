92af934784d7436bd7930e333ce9e96acb3ddf3c
akaJuliaan <info@versahl.at>
2024-04-10 09:57:23 +0200

    add timeout for pipes

--- WinService/PipeClient.cs
+++ WinService/PipeClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO.Pipes;
+using TimeoutException = System.ServiceProcess.TimeoutException;
 
 namespace WinService;
 
@@ -9,7 +10,7 @@ public class PipeClient
     public static async Task SendCommand(string pipeName, string command, CancellationToken token)
     {
         var clientPipe = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut, PipeOptions.Asynchronous);
-        await clientPipe.ConnectAsync(token);
+        await clientPipe.ConnectAsync(5000, token);
 
         var writer = new StreamWriter(clientPipe)
         {
@@ -17,22 +18,45 @@ public class PipeClient
         };
 
         var reader = new StreamReader(clientPipe);
-        var line = await reader.ReadLineAsync(token);
-
-        if (line != "ClassInsights")
+        var readTask = reader.ReadLineAsync(token).AsTask();
+        
+        // check if pipe is from ClassInsights
+        if (await ExecuteOperationWithTimeOut(readTask, 5000, token) != "ClassInsights")
         {
             Logger.Error($"{pipeName} is from another application!");
             return;
         }
 
-        await writer.WriteLineAsync(command);
-
-        line = await reader.ReadLineAsync(token);
-        if (line is not "OK") Logger.Error($"{line} for {pipeName} did not succeed!");
+        // send command to WinClient
+        var writeTask = writer.WriteLineAsync(command);
+        await ExecuteOperationWithTimeOut(writeTask, 5000, token);
         
-        await writer.WriteLineAsync("BYE");
+        // check if command was successful
+        if (await ExecuteOperationWithTimeOut(reader.ReadLineAsync(token).AsTask(), 5000, token) is { } line && line != "OK")
+            Logger.Error($"{line} for {pipeName} did not succeed!");
+
+        await ExecuteOperationWithTimeOut(writer.WriteLineAsync("BYE"), 5000, token);
         clientPipe.WaitForPipeDrain();
 
         clientPipe.Close();
+    }
+    
+
+    private static async Task ExecuteOperationWithTimeOut(Task operationTask, int timeout, CancellationToken token)
+    {
+        var completedTask = await Task.WhenAny(operationTask, Task.Delay(timeout, token));
+        if (completedTask != operationTask)
+            throw new TimeoutException("Task timed out!");
+
+        await operationTask;
+    }
+    
+    private static async Task<T> ExecuteOperationWithTimeOut<T>(Task<T> operationTask, int timeout, CancellationToken token)
+    {
+        var completedTask = await Task.WhenAny(operationTask, Task.Delay(timeout, token));
+        if (completedTask != operationTask)
+            throw new TimeoutException("Task timed out!");
+
+        return await operationTask;
     }
 }
